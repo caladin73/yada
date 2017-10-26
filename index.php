8f145ca737b3b3d987b85ca49f827b89dcaf73c9
JecraDK <Jesperuthkrab@hotmail.com>
2017-10-26 10:00:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+
+        <?php
+            session_start();
+            require_once './controller/Controller.inc.php'; // domainmodel
+
+            $controller = new Controller($_GET, $_POST, $_FILES);
+            $controller->action();
+        ?>
+
